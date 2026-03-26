7b4829efa4a214dbc7f4693a0aebcae1bd17f7a4
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-03-26 15:25:01 -0300

    feat(app): cria o primeiro get e testa a conexao

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+# instanciando a classe FastAPI
+app = FastAPI()
+
+# Criando rota
+@app.get('/')  # definimos o verbo HTTP que vamos usar
+def read_root():
+    return {"message": "Hello, Word!"}d
