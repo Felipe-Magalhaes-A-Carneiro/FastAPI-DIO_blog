e1967a9ebd108169ab0726c8f72e8c254c4a9da2
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-03-31 11:15:42 -0300

    docs(03-cartilha): passagem de parametros por tipo em path

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from datetime import datetime, UTC
 app = FastAPI()
 
 # Criando rota
-@app.get('/posts/{framework}')  # definimos o verbo HTTP que vamos usar
-def read_posts(framework):
+@app.get('/posts/{framework}')  # definimos que o framework que utilizaremos é qualquer um que dermos no path
+def read_posts(framework: str): # definimos que o framework usado é uma string, o que ele aceita até um string como '10' como um path
     return {
         "posts": [
                 {'title': f'Criando uma aplicação com {framework}' , 'date': datetime.now(UTC)},
