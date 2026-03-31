58915c825a1d00bd5c222400b131f5415ba2d48e
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-03-31 11:48:21 -0300

    feat(path): cria nova rota post

--- app/main.py
+++ app/main.py
@@ -4,9 +4,24 @@ from datetime import datetime, UTC
 # instanciando a classe FastAPI
 app = FastAPI()
 
+# Criando uma simulação de data base
+fake_db = [
+    {'title': 'Criando uma aplicação com Django' , 'date': datetime.now(UTC)},
+    {'title': 'Internacionalizando uma app FastAPI' , 'date': datetime.now(UTC)},
+    {'title': 'Internacionalizando uma app Flask' , 'date': datetime.now(UTC)},
+    {'title': 'Internacionalizando uma app Starlett' , 'date': datetime.now(UTC)}
+
+]
+
+#Criando novo método
+@app.get('/posts')
+def read_posts():
+    return fake_db
+
+
 # Criando rota
-@app.get('/posts/{framework}')  # definimos que o framework que utilizaremos é qualquer um que dermos no path
-def read_posts(framework: str): # definimos que o framework usado é uma string, o que ele aceita até um string como '10' como um path
+@app.get('/posts/{framework}')  
+def read_framework_posts(framework: str): 
     return {
         "posts": [
                 {'title': f'Criando uma aplicação com {framework}' , 'date': datetime.now(UTC)},
