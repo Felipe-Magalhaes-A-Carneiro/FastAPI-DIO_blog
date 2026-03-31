e782fd4726175f8a65390b14efcad7d35e9a1642
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-03-31 10:31:07 -0300

    test(post/django): inicializa posts com data e hora

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from datetime import datetime, UTC
 
 # instanciando a classe FastAPI
 app = FastAPI()
 
 # Criando rota
-@app.get('/')  # definimos o verbo HTTP que vamos usar
-def read_root():
-    return {"message": "Hello, Word!"}
+@app.get('/posts/{framework}')  # definimos o verbo HTTP que vamos usar
+def read_posts():
+    return {"posts": [
+                {'title': 'Criando uma aplicação com {framework}' , 'date': datetime.now(UTC)},
+                
+                {'title': 'Internacionalizando uma app {framework}' , 'date': datetime.now(UTC)}
+            
+            ]
+    } # retorna uma lista de posts
