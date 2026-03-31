740fdbc82af64a1a59a0f3495cbe6e86efdae4b5
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-03-31 11:02:31 -0300

    docs(03-cartilha): explica estilos de formatacao

--- app/main.py
+++ app/main.py
@@ -6,11 +6,12 @@ app = FastAPI()
 
 # Criando rota
 @app.get('/posts/{framework}')  # definimos o verbo HTTP que vamos usar
-def read_posts():
-    return {"posts": [
-                {'title': 'Criando uma aplicação com {framework}' , 'date': datetime.now(UTC)},
+def read_posts(framework):
+    return {
+        "posts": [
+                {'title': f'Criando uma aplicação com {framework}' , 'date': datetime.now(UTC)},
                 
-                {'title': 'Internacionalizando uma app {framework}' , 'date': datetime.now(UTC)}
+                {'title': f'Internacionalizando uma app {framework}' , 'date': datetime.now(UTC)}
             
             ]
     } # retorna uma lista de posts
